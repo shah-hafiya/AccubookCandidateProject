4ea8d27ea3bb15337a0b3b9d410756b19cfc20b1
George Ilie <georgeilie@live.com>
2021-02-25 12:11:32 +0000

    Add project files.

---
+++ AccubookCandidateProject/Controllers/BookingsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace AccubookCandidateProject.Controllers
+{
+    public class BookingsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+
+    public class BookingsApiController : ApiController
+    {
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("api/bookings")]
+        public async Task<IHttpActionResult> GetBookings()
+        {
+            // your code goes here
+
+            return Ok();
+        }
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("api/booking")]
+        public async Task<IHttpActionResult> PostBooking()
+        {
+            // your code goes here
+
+            return Ok();
+        }
+    }
+}
---
+++ AccubookCandidateProject/Controllers/HotelsController.cs
@@ -0,0 +1,32 @@
+﻿using AccubookCandidateProject.Data;
+using AccubookCandidateProject.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace AccubookCandidateProject.Controllers
+{
+    public class HotelsController : Controller
+    {
+        public ActionResult Index()
+        {
+            HotelContext db = new HotelContext();
+            List<HotelDTO> hotelDTOs = new List<HotelDTO>();
+
+            foreach (Hotel h in db.Hotels)
+            {
+                HotelDTO dto = new HotelDTO
+                {
+                    Id = h.Id,
+                    Name = h.Name,
+                    Address = h.Address,
+                    TimesBooked = db.Bookings.Where(b => b.HotelId == h.Id).Count(),
+                };
+            }
+
+            return View();
+
+            // your code here - optimize existing code & add any missing code
+        }
+    }
+}
